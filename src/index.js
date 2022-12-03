9eaaae4b34fb97efd89fa42e035fff99c8f7361e
Franzi1005 <fschallhorn@gmail.com>
2022-12-03 13:14:10 +0100

    Created Weather App HTML, CSS and JS incl. Search Engine
    The days shown in the overview still need updating

---
+++ src/index.js
@@ -0,0 +1,107 @@
+let now = new Date();
+
+let allMonths = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let date = now.getDate();
+let month = allMonths[now.getMonth()];
+let year = now.getFullYear();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let weekday = weekdays[now.getDay()];
+let completeDate = document.querySelector("small");
+
+completeDate.innerHTML = `${weekday}, ${month} ${date} ${year}, ${hours}:${minutes}`;
+
+// challenge 2
+
+let searchForm = document.querySelector("#city-input");
+let cityInput = document.querySelector("#city-input-field");
+
+function changeTemperature(response) {
+  let heading = document.querySelector("h1");
+  let newCity = response.data.name;
+  heading.innerHTML = `${newCity}`;
+  let temperature = document.querySelector("#temperature");
+  let actualTemp = Math.round(response.data.main.temp);
+  temperature.innerHTML = actualTemp;
+  let weatherCond = document.querySelector("#weather-condition");
+  let text = response.data.weather[0].description;
+  weatherCond.innerHTML = `${text}`;
+}
+
+function changeCity(city) {
+  let apiKey = "8a869017a9bbe9c440c0fea9e1fa0af6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(changeTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input-field").value;
+  changeCity(city);
+}
+
+searchForm.addEventListener("submit", handleSubmit);
+
+changeCity("New York");
+
+// challenge 3
+
+function getTempAndCity(response) {
+  let temperature = document.querySelector("#temperature");
+  let actualTemp = Math.round(response.data.main.temp);
+  temperature.innerHTML = actualTemp;
+  let heading = document.querySelector("h1");
+  let cityName = response.data.name;
+  heading.innerHTML = cityName;
+  let weatherCond = document.querySelector("#weather-condition");
+  let text = response.data.weather[0].description;
+  weatherCond.innerHTML = `${text}`;
+  temperature.innerHTML = actualTemp;
+}
+
+function showPosition(position) {
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let apiKey = "8a869017a9bbe9c440c0fea9e1fa0af6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(getTempAndCity);
+}
+
+function showLocalPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocBtn = document.querySelector("#current-location");
+
+currentLocBtn.addEventListener("click", showLocalPosition);
